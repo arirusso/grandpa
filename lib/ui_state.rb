99b0acad64fd240465e61bc7cfd4a124d21b26d8
Ari Russo <ari.russo@gmail.com>
2010-06-22 22:54:02 -0400

    comments, fixed old style callback call

--- lib/ui_state.rb
+++ lib/ui_state.rb
@@ -9,13 +9,15 @@ class Grandpa::UiState
     @selection = []
   end
   
+  # delete all of the selected models
   def delete_selected
     @selection.each { |s| s.destroy! }
     deselect_all
   end
   
+  # clear the selection queue
   def deselect_all
-    @selection.each { |model| model.deselect_proc.call({}) }
+    @selection.each { |model| model.behavior.deselect.call(:model => model) }
     @selection.clear
   end
   
