f81efefd28e53a7c094624ee8f5ac4efdd596902
Ari Russo <ari.russo@gmail.com>
2010-06-22 23:11:42 -0400

    comments

--- lib/view.rb
+++ lib/view.rb
@@ -24,27 +24,32 @@ class Grandpa::View
     @associated = options[:associate_with]
   end
   
+  # the top-left-most position values for its components
   def location
     Point[@components.values.map { |c| c.location.x }.min,@components.values.map { |c| c.location.y }.min]
   end
   
+  # the bottom-right-most position values for its components
   def bounds
     Point[@components.values.map { |c| c.bounds.x }.max,@components.values.map { |c| c.bounds.y }.max]
   end
   
-  def intersects?(visible_state)
+  # do any of my components intersect components from the passed in view? 
+  def intersects?(view)
     @components.each_value do |component|
-      visible_state.components.each_value do |other_comp|
+      view.components.each_value do |other_comp|
         return true if component.contains_shape?(other_comp)
       end
     end
     false
   end
   
+  # let my components know that model properties have been updated
   def update
     @components.each_value { |component| component.update }
   end
   
+  # draw my components
   def draw(window, model, show_label=false)
     @components.each_value { |component| component.draw(window) }
     if !@label.nil? and (show_label or @label[:always_on]) 
@@ -55,6 +60,7 @@ class Grandpa::View
     end
   end
   
+  # the gosu window has been initialized, send it to the components in case they need it
   def handle_window_initialization(window)
     @components.each_value { |component| component.init(window) if component.respond_to?('init') }
   end
